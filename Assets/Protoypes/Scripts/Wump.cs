eabfd1799bc72edf154fa9b89dfdfff61268098d
GUIMê <gpimenteldesales@gmail.com>
2026-08-11 15:29:29 -0300

    CommitMecanicaWumpPrototype

--- Assets/Protoypes/Scripts/Wump.cs
+++ Assets/Protoypes/Scripts/Wump.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Wump : MonoBehaviour
@@ -6,16 +7,42 @@ public class Wump : MonoBehaviour
     public float speed = 7f;
     Rigidbody2D physicsPlayer;
     Vector2 move;
+    bool isGrounded = true;
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashTime = 0.2f;
+    private float dashPower = 24f;
+    private float dashCooldown = 1f;
+
+    [SerializeField] private TrailRenderer tr;
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = physicsPlayer.gravityScale;
+        physicsPlayer.gravityScale = 0f;
+        physicsPlayer.linearVelocity = new Vector2(transform.localScale.x * dashPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        tr.emitting = false;
+        physicsPlayer.gravityScale = originalGravity;
+    }
+
     void Start()
     {
         physicsPlayer = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        
+       
     }
     void FixedUpdate()
     {
+
+        
+
+        //
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         move = new Vector2(moveX, moveY).normalized;
@@ -28,6 +55,9 @@ public class Wump : MonoBehaviour
     }
     void JumpPlayer()
     {
-        physicsPlayer.linearVelocity = new Vector2(physicsPlayer.linearVelocity.x, jump);
+        if (isGrounded == true)
+        {
+            physicsPlayer.linearVelocity = new Vector2(physicsPlayer.linearVelocity.x, jump);
+        }
     }
 }
